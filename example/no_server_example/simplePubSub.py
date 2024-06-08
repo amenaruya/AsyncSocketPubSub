3448291e7c279add52dbebb2a6d1bdfc9c9da3f4
amenaruya <132125305+amenaruya@users.noreply.github.com>
2024-06-09 01:11:10 +0900

    Add files via upload

---
+++ example/no_server_example/simplePubSub.py
@@ -0,0 +1,28 @@
+from AsyncSocketPubSub.server import PubSubClients
+from threading import Thread
+import time
+
+CHANNEL = "C"
+
+def Subscriber():
+    subClient = PubSubClients(id = "subscriber")
+    while True:
+        data = subClient.subscribe(channel = CHANNEL)
+        print(data)
+
+def Publisher():
+    pubClient = PubSubClients(id = "publisher")
+    while True:
+        pubClient.publish(
+            channel = CHANNEL,
+            payload = "hello?"
+        )
+        time.sleep(3)
+
+sub = Thread(
+    target = Subscriber,
+    daemon = True
+)
+sub.start()
+
+Publisher()
---
+++ example/server_example/server.py
@@ -0,0 +1,6 @@
+from AsyncSocketPubSub import runServer
+
+runServer(
+    host = "0.0.0.0",
+    port = 18883
+)
